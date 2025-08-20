512021ec5a490876304e760dd1c3f831552df709
git-Sahitya <sahitya5289@gmail.com>
2025-08-20 19:41:54 +0530

    Implement routing and add page components for Home, BuyCredit, and Result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import BuyCredit from "./pages/BuyCredit";
+import Result from "./pages/Result";
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
