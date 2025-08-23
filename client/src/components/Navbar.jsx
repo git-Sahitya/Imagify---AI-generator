2df00132df2a4801b8112c857c064c5338c39268
git-Sahitya <sahitya5289@gmail.com>
2025-08-23 19:48:29 +0530

    Refactor Navbar and AppContext for improved readability; add Header component to Home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
-  const {user} = useContext(AppContext)
+  const { user } = useContext(AppContext);
   const navigate = useNavigate();
   return (
     <div className="flex items-center justify-between py-4">
@@ -14,9 +14,14 @@ const Navbar = () => {
       <div>
         {user ? (
           <div className="flex items-center gap-2 sm:gap-3">
-            <button onClick={()=> navigate('/buy')} className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700">
+            <button
+              onClick={() => navigate("/buy")}
+              className="flex items-center gap-2 bg-blue-100 px-4 sm:px-6 py-1.5 sm:py-3 rounded-full hover:scale-105 transition-all duration-700"
+            >
               <img className="w-5 " src={assets.credit_star} alt="" />
-              <p className="text-xs sm:text-sm font-medium text-gray-600">Credit left : 50</p>
+              <p className="text-xs sm:text-sm font-medium text-gray-600">
+                Credit left : 50
+              </p>
             </button>
             <p className="text-gray-600 max-sm:hidden">Hi , Sahitya Singh</p>
             <div className="relative group">
@@ -34,12 +39,7 @@ const Navbar = () => {
           </div>
         ) : (
           <div className="flex items-center gap-2 sm:gap-5 ">
-            <p
-              onClick={() => {
-                navigate("/buy");
-              }}
-              className="cursor-pointer"
-            >
+            <p onClick={() => {navigate("/buy");}} className="cursor-pointer">
               Pricing
             </p>
             <button className="bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full">
