46bbcd71a72c75441b97758d29604121a8296c9d
git-Sahitya <sahitya5289@gmail.com>
2025-09-13 19:38:55 +0530

    Implement login functionality with user authentication and error handling

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -29,3 +29,29 @@ const registerUser = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User does not exist" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      res.json({ success: true, token, user: { name: user.name } });
+    } else {
+      return res.json({ success: false, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+export {registerUser,loginUser}
--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
-import express, { json } from 'express'
-import cors from "cors"
-import dotenv from "dotenv"
-import connectDB from './config/db.js';
+import express, { json } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRouter from "./routes/user.route.js";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT;
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-await connectDB()
+app.use(cors());
+app.use(express.json());
+await connectDB();
 
+app.use('/api/user' , userRouter)
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
-  app.get('/' , (req,res)=>{
-    res.send("API working")
-  })
-
-
-app.listen( PORT , ()=>{
-console.log(`Server is running on port ${PORT}`);
-
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
